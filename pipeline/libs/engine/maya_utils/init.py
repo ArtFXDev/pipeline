75e9a3d6b95b48f240a775c10150e1559eb76620
Soulayrol <sylvain.soulayrol@gmail.com>
2021-04-07 19:07:54 +0200

    add init maya

---
+++ pipeline/libs/engine/maya_utils/init.py
@@ -0,0 +1,41 @@
+from spil.libs.util import log
+from pipeline import conf
+from spil.conf import fs_conf
+from pipeline.libs.manager.entities import Entities
+import maya.mel as mel
+
+def init_scene():
+    log.info("Initialisation de la scene maya")
+
+    entity = Entities()
+    sid = entity.get_engine_sid()
+    if sid.has_a("ext"):
+        # Shelf
+        project_path = sid.path.split("03_WORK_PIPE")[0]
+        shelf_path = project_path + "03_WORK_PIPE/04_TOOLS/01_SHELVES"
+        if os.path.exists(shelf_path):
+            files = os.listdir(shelf_path)
+            for f in files:
+                path_file = shelf_path + "/" + f
+                if os.path.isfile(path_file):
+                    ext = f.split(".")[-1]
+                    if ext == "mel":
+                        log.info("import file " + f)
+                        try:
+                            mel_cmd = 'loadNewShelf "' + path_file + '"'
+                            mel.eval(mel_cmd)
+                        except RuntimeError:
+                            log.warn("shelf, not loaded correctly, it's may have been already loaded")
+        # Workspace
+        project = ''
+        for key, value in fs_conf.path_mapping['project'].items():
+            if value == sid.project:
+                project = key
+        if str(sid).split('/')[1] == 's':
+            workspace_path = conf.shot_workspace_path.format(
+                root=conf.root, project=project, dimension='3d')
+        else:
+            workspace_path = conf.asset_workspace_path.format(root=conf.root, project=project,
+                                                              cat=sid.cat, name=sid.name)
+        entity.engine.set_workspace(workspace_path)
+        log.info("workspace set to {}".format(workspace_path))
